edc5ded39699646a7e1e5304ecaca0ce5c8a1965
Armynerh <aimenah@outlook.com>
2023-10-13 17:20:40 -0400

    fix changes

--- person.rb
+++ person.rb
@@ -3,23 +3,23 @@ require_relative 'rental'
 
 class Person
   attr_accessor :name, :age, :rentals
-  
-  def initialize(age, name = 'Unknown', parent_permission = true)
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = generate_id
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
   end
-  
+
   def add_rental(book, date)
     Rental.new(date, book, self)
   end
-  
+
   private
 
-    def generate_unique_id
-    used_ids = Set.new 
+  def generate_unique_id
+    used_ids = Set.new
     loop do
       new_id = rand(1000..9999)
       unless used_ids.include?(new_id)
@@ -40,6 +40,4 @@ class Person
   def correct_name
     name
   end
-
-
 end
